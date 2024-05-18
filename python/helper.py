3b0fead5b094fa571f139dcc3470a8d6e68a3f5f
Archana <archanabadarinarayan11@gmail.com>
2024-05-18 23:56:02 +0530

    added new files

---
+++ python/helper.py
@@ -0,0 +1,48 @@
+import speech_recognition as sr
+import google.generativeai as genai
+import os
+from gtts import gTTS
+
+
+GOOGLE_API_KEY = "AIzaSyCCHWiz4-QMHACAyHJ3mh8SzvqpaVZOLt8"
+
+def voice_input():
+    # Create a recognizer instance
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = r.listen(source)
+
+    try:
+        text = r.recognize_google(audio)  # Using Google Speech Recognition
+        print("You said:", text)
+        return text
+    except sr.UnknownValueError:
+        print("Sorry, could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+
+    
+
+def text_to_speech(text):
+    # Create a gTTS object
+    tts = gTTS(text=text, lang='en')  # Language can be changed
+
+    # Save the audio as an MP3 file
+    tts.save("speech.mp3")
+
+
+
+def llm_model_object(user_text):
+    genai.configure(api_key=GOOGLE_API_KEY)
+    
+    model = genai.GenerativeModel('gemini-pro')
+    
+    response=model.generate_content(user_text)
+    
+    result=response.text
+    
+    return result
+    
